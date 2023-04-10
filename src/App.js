e3c1686320233d22d5dee7ac24fa14728fe3d47b
jonathanphill <jonathanphill@gmail.com>
2023-04-10 19:14:18 -0400

    making a fetch request and managing states of data-loading, success, errorM

--- src/App.js
+++ src/App.js
@@ -14,17 +14,30 @@ function GithubUser({name, location, avatar}){
 }
 function App() {
   const [data, setData] = useState(null);
+  const[error, setError] = useState(null);
+  const[loading, setLoading] = useState(false);
+
+  // Thre possible states the data may be in
+// Loading State
+// Success State
+// Error State
+
   useEffect(() => {
+    setLoading(true);
     fetch(`https://api.github.com/users/moonhighway`)
       .then((respnse) => respnse.json())
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
   // <pre>{JSON.stringify(data, null, 2)}</pre>
-  if (data) 
+ if(loading) return <h1>Loading...</h1>;
+ if(error) return <pre>{JSON.stringify(error)}</pre>;
+ if(!data) return null;
   return(
     <GithubUser name={data.name} location={data.location} avatar={data.avatar_url}/>
   )
-  return <h1>Data</h1>;
+
 }
 
 export default App;
