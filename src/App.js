067a9a30c605563b913f31e4fd632e77b0229e9d
jonathanphill <jonathanphill@gmail.com>
2023-04-10 19:04:59 -0400

    making a fetch request and adding to the DOM

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import "./App.css";
 import {useState, useEffect } from "react";
 
 
-function useInput(initialValue) {
-  const [value, setValue] = useState(initialValue);
-  return [
-    {value, onChange: (e) => setValue(e.target.value)},
-    ()=> setValue(initialValue)
-  ];
+function GithubUser({name, location, avatar}){
+  return(
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name} />
+    </div>
+  )
+
 }
 function App() {
-  const [data, setData] =useState(null);
-  useEffect(()=> {
+  const [data, setData] = useState(null);
+  useEffect(() => {
     fetch(`https://api.github.com/users/moonhighway`)
-    .then(respnse => respnse.json())
-    .then(setData)
-  },[])
-  if(data) return <pre>{JSON.stringify(data, null, 2)}</pre>
-  return (
-   <h1>Data</h1>
-  );
+      .then((respnse) => respnse.json())
+      .then(setData);
+  }, []);
+  // <pre>{JSON.stringify(data, null, 2)}</pre>
+  if (data) 
+  return(
+    <GithubUser name={data.name} location={data.location} avatar={data.avatar_url}/>
+  )
+  return <h1>Data</h1>;
 }
 
 export default App;
