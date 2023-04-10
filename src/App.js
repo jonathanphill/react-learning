bc772353026f458f6115ff10d3406292cbaa3b1f
jonathanphill <jonathanphill@gmail.com>
2023-04-10 18:55:40 -0400

    making a fetch request using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import {useState } from "react";
+import {useState, useEffect } from "react";
+
 
 function useInput(initialValue) {
   const [value, setValue] = useState(initialValue);
@@ -9,20 +10,15 @@ function useInput(initialValue) {
   ];
 }
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value}, ${colorProps.value}`);
-    resetTitle();
-    resetColor();
-  };
+  const [data, setData] =useState(null);
+  useEffect(()=> {
+    fetch(`https://api.github.com/users/moonhighway`)
+    .then(respnse => respnse.json())
+    .then(setData)
+  },[])
+  if(data) return <pre>{JSON.stringify(data, null, 2)}</pre>
   return (
-    <form onSubmit={submit}>
-      <input {...titleProps} type="text" placeholder="color, title..." />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
+   <h1>Data</h1>
   );
 }
 
